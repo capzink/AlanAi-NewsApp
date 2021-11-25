cd674dfe30ef16550912cf9009580f92789c3a51
Carlos <capz2003@gmail.com>
2021-11-25 12:57:29 -0500

    first commit

---
+++ projects/project1/src/App.js
@@ -0,0 +1,26 @@
+
+import
+import './App.css';
+
+function App() {
+  
+  const [ people, setpeople] =
+
+  return (
+    <>
+    <div className="container">
+      <div className="card">
+        <h2>5 Birthdays today</h2>
+        
+      </div>
+
+
+
+    </div>
+
+      
+    </>
+  );
+}
+
+export default App;
