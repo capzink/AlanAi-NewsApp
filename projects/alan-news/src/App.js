a7c10b64d947b3994a729e8a2d9840437d1a6a50
Carlos <capz2003@gmail.com>
2022-01-03 09:44:28 -0500

    Added Alan file

--- projects/alan-news/src/App.js
+++ projects/alan-news/src/App.js
@@ -7,6 +7,7 @@ import wordsToNumbers from "words-to-numbers";
 
 
 
+
 const alankey = "c006edb76096c08abf8044fe3d2a2ba52e956eca572e1d8b807a3e2338fdd0dc/stage";
 const App =()=>{
 const classes= useStyles()
