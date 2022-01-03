41d27ecd099bd7d81dd8032f8b8c7fc292166d9e
Carlos <capz2003@gmail.com>
2022-01-02 20:47:10 -0500

    Adds news Api cnn articles with voice

--- projects/alan-news/src/App.js
+++ projects/alan-news/src/App.js
@@ -1,13 +1,15 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alankey = "c006edb76096c08abf8044fe3d2a2ba52e956eca572e1d8b807a3e2338fdd0dc/stage";
 const App =()=>{
+    const [news, setNews]=useState([])
 
     useEffect(()=>{
         alanBtn({ key:alankey, onCommand: ({command, articles})=>{
             if(command === 'newHeadlines'){
-                
+                setNews(articles);
             }
         }
         })
@@ -15,6 +17,7 @@ const App =()=>{
     return(
         <div>
             <h1>Alan Ai news App</h1>
+            <NewsCards articles={setNews}/>
         </div>
     )
 }
---
+++ projects/alan-news/src/components/NewsCards/NewsCards.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NewsCards = () => {
+    return (
+        <div>
+            <h1>News Cards</h1>
+        </div>
+    )
+}
+export default NewsCards
